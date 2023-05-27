ede1407424c68c887bcaf1d01cf1d13b89a38b1a
NasaJPL123 <Ankhangngoquoc@gmail.com>
2023-05-27 18:24:18 +0700

    a

---
+++ login/login.js
@@ -0,0 +1,19 @@
+function validateForm() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+  
+    var storedUserData = localStorage.getItem('userData');
+    if (storedUserData) {
+      storedUserData = JSON.parse(storedUserData);
+  
+      if (email === storedUserData.email && password === storedUserData.password) {
+        alert('Login successful!');
+      } else {
+        alert('Invalid email or password. Please try again.');
+      }
+    } else {
+      alert('User data not found. Please register first.');
+    }
+  }
+  
+  
--- signin/signin.js
+++ signin/signin.js
@@ -37,3 +37,42 @@ function validateForm() {
         confirmPasswordError.textContent = 'Confirm password is required';
     }
 }
+
+var isValid = true;
+
+  if (!firstName) {
+    firstNameError.textContent = 'First name is required';
+    isValid = false;
+  }
+
+  if (!lastName) {
+    lastNameError.textContent = 'Last name is required';
+    isValid = false;
+  }
+
+  if (!email) {
+    emailError.textContent = 'Email is required';
+    isValid = false;
+  }
+
+  if (!password) {
+    passwordError.textContent = 'Password is required';
+    isValid = false;
+  }
+
+  if (!confirmPassword) {
+    confirmPasswordError.textContent = 'Confirm password is required';
+    isValid = false;
+  }
+
+  if (isValid) {
+    var userData = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
+    };
+    localStorage.setItem('userData', JSON.stringify(userData));
+
+    window.location.href = '/login/login.html';
+  }
